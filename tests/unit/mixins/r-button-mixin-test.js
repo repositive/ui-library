d102a59571474f54aff1a70dd067deec0a5b0ca5
Tom Byers <hello@tombye.rs>
2017-06-15 18:38:04 +0100

    Comment out old logic tests

--- tests/unit/mixins/r-button-mixin-test.js
+++ tests/unit/mixins/r-button-mixin-test.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 import sinon from 'sinon';
 import RButtonMixin from 'ui-library/mixins/r-button-mixin';
 
-const { get, set } = Ember;
+const { get } = Ember;
 
 describe('Unit | Mixin | r-button-mixin', function() {
   let subject;
@@ -38,8 +38,8 @@ describe('Unit | Mixin | r-button-mixin', function() {
       ]);
     });
 
-    it('should have correct options set', function () {
-      expect(get(subject, 'options')).to.be.eql({
+    it('should have correct attributes set', function () {
+      expect(get(subject, 'attributes')).to.be.eql({
         variant: ['primary', 'secondary', 'cancel'],
         size: ['small', 'big']
       });
@@ -53,59 +53,59 @@ describe('Unit | Mixin | r-button-mixin', function() {
     });
   });
 
-  describe('methods', function () {
-    describe('init', function () {
-      const method = this.title;
-
-      it('should call _setOptionFlag for each key in options property', function () {
-        set(subject, '_setOptionFlag', sinon.spy());
-
-        subject[method]();
-
-        Object.keys(get(subject, 'options')).forEach(key => {
-          expect(get(subject, '_setOptionFlag').calledWith(key)).to.be.equal(true);
-        });
-      });
-    });
-
-    describe('_setOptionFlag', function () {
-      const method = this.title;
-      const flagType = 'size';
-      const flagName = 'big';
-
-      it('should set given flagName', function () {
-        subject.setProperties({
-          _isValidFlagName: sinon.stub().returns(true),
-          [flagType]: flagName
-        });
-
-        subject[method](flagType);
-
-        expect(get(subject, flagName)).to.be.equal(true);
-      });
-
-      it('should set default flagName', function () {
-        subject.setProperties({
-          _isValidFlagName: sinon.stub().returns(false),
-          [flagType]: flagName
-        });
-
-        subject[method](flagType);
-
-        expect(get(subject, get(subject, `defaults.${flagType}`))).to.be.equal(true);
-      })
-    });
-
-    describe('_isValidFlagName', function () {
-      const method = this.title;
-
-      it('should return true', function () {
-        expect(subject[method]('size', 'big')).to.be.equal(true);
-      });
-
-      it('should return false', function () {
-        expect(subject[method]('size', 'foo-bar')).to.be.equal(false);
-      });
-    });
-  });
+  // describe('methods', function () {
+  //   describe('init', function () {
+  //     const method = this.title;
+  //
+  //     it('should call _setOptionFlag for each key in options property', function () {
+  //       set(subject, '_setOptionFlag', sinon.spy());
+  //
+  //       subject[method]();
+  //
+  //       Object.keys(get(subject, 'options')).forEach(key => {
+  //         expect(get(subject, '_setOptionFlag').calledWith(key)).to.be.equal(true);
+  //       });
+  //     });
+  //   });
+  //
+  //   describe('_setOptionFlag', function () {
+  //     const method = this.title;
+  //     const flagType = 'size';
+  //     const flagName = 'big';
+  //
+  //     it('should set given flagName', function () {
+  //       subject.setProperties({
+  //         _isValidFlagName: sinon.stub().returns(true),
+  //         [flagType]: flagName
+  //       });
+  //
+  //       subject[method](flagType);
+  //
+  //       expect(get(subject, flagName)).to.be.equal(true);
+  //     });
+  //
+  //     it('should set default flagName', function () {
+  //       subject.setProperties({
+  //         _isValidFlagName: sinon.stub().returns(false),
+  //         [flagType]: flagName
+  //       });
+  //
+  //       subject[method](flagType);
+  //
+  //       expect(get(subject, get(subject, `defaults.${flagType}`))).to.be.equal(true);
+  //     })
+  //   });
+  //
+  //   describe('_isValidFlagName', function () {
+  //     const method = this.title;
+  //
+  //     it('should return true', function () {
+  //       expect(subject[method]('size', 'big')).to.be.equal(true);
+  //     });
+  //
+  //     it('should return false', function () {
+  //       expect(subject[method]('size', 'foo-bar')).to.be.equal(false);
+  //     });
+  //   });
+  // });
 });
