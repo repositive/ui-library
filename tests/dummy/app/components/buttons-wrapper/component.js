07a05c2924add2771f3699f9ea40769218203de5
Sebastian Place <sebplace94@gmail.com>
2017-06-16 15:10:24 +0100

    Add all buttons in dummy app

---
+++ tests/dummy/app/components/buttons-wrapper/component.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import layout from './template';
+
+const { Component, RSVP } = Ember;
+
+export default Component.extend({
+  layout,
+  isInvalid: false,
+
+  actions: {
+    save() {
+      return this._fakeRequest();
+    }
+  },
+
+  _fakeRequest() {
+    return new RSVP.Promise((resolve) => {
+      setTimeout(() => { resolve('OK'); }, 1500);
+    });
+  }
+});
